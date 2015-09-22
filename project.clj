43a5f682af4f304bfa19cbda1a25cfe9e3948cfd
Jenkins CI <ci@puppetlabs.com>
2015-09-22 11:33:39 -0700

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (def tk-version "1.1.1")
 (def tk-jetty-version "1.4.1")
 
-(defproject puppetlabs/trapperkeeper-authorization "0.1.3-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-authorization "0.1.3"
   :description "Trapperkeeper authorization system"
   :url "http://github.com/puppetlabs/trapperkeeper-authorization"
   :license {:name "Apache License, Version 2.0"
